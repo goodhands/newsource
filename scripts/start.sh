cb133cd8c925729515cd3a7aae5991f4a4cb293d
Samuel Olaegbe <25031528+goodhands@users.noreply.github.com>
2025-11-09 21:01:40 -0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,3 +13,11 @@ php artisan migrate --force
 
 echo "Seeding database"
 php artisan db:seed --force
+
+# Start scheduler & queue worker
+echo "Starting scheduler and queue worker..."
+nohup php artisan schedule:work > /dev/null 2>&1 &
+nohup php artisan queue:work --tries=3 > /dev/null 2>&1 &
+
+# Keep container running (Nginx/PHP-FPM)
+exec "$@"
